022c0f1d5d90c36225df356ee6a89a4170308b6c
jting130 <jamie940130@gmail.com>
2025-04-10 15:07:48 +0800

    Update 2.login.php

--- 2.login.php
+++ 2.login.php
@@ -1,5 +1,5 @@
 <?php 
-    echo $_POST["id"];
-    echo "<br>";
-    echo $_POST["pwd"];
+    echo $_POST["id"]; //顯示從表單傳過來的帳號資料（name="id"）
+    echo "<br>"; //顯示 HTML 的換行標籤，讓帳號與密碼分開顯示
+    echo $_POST["pwd"]; //顯示從表單傳過來的密碼資料（name="pwd"）
 ?>
