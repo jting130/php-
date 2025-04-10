c812b2cf1bb00a6971108f5c90b8d9ce8154c99d
jting130 <jamie940130@gmail.com>
2025-04-10 15:15:16 +0800

    Update 1.html.php

--- 1.html.php
+++ 1.html.php
@@ -1,7 +1,7 @@
 <html> <HTML 文件開始>
     <head><title>html+php</title></head> < 網頁標題（會顯示在瀏覽器的分頁上）>      
     <body>
-        <?php echo "Hello world!"; ?>   < PHP 程式碼：輸出 "Hello world!" 到網頁上 
+        <?php echo "Hello world!"; ?>   < PHP 程式碼：輸出 "Hello world!" 到網頁上 >
         <h1>html</h1> < 顯示 HTML 的標題文字，大小為 h1 >
         <?php echo "Hello world!"; ?> < 再次輸出 "Hello world!" >
         <h1>html</h1> < 再次顯示 h1 標題 >
